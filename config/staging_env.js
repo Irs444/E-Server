4660232f5a1ce008cd9ece427740f15d2e066506
atharv03 <atharvchauhan03@gmail.com>
2020-09-09 01:20:04 +0530

    change db

--- config/staging_env.js
+++ config/staging_env.js
@@ -4,8 +4,8 @@
 
 module.exports = {
   // db: process.env.MONGO_URL ||  'mongodb://myUserAdmin:abc123@localhost:27017/licApp?authSource=admin',
-  db: process.env.MONGO_URL || 'mongodb://localhost:27017/licApp',
-  logDir: './logs/', //@todo : check if log directory exits, if not create one.
+  db: process.env.MONGO_URL || 'mongodb://localhost:27017/arab_tech_store"',
+  logDir: "./logs/", //@todo : check if log directory exits, if not create one.
   sessionSecret: "thisisareallylongandbigsecrettoken",
-  encryptionSecret: 'securetokentoencrypt',
- };
+  encryptionSecret: "securetokentoencrypt",
+};
