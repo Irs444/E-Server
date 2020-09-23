728b99df04514cf9315ef33dfa89e93569c06cc7
atharv03 <atharvchauhan03@gmail.com>
2020-09-23 17:35:09 +0530

    save changes

--- app/libs/mail.js
+++ app/libs/mail.js
@@ -11,41 +11,42 @@ password- zG&LYE4\dp?LA]rJ
 http://mail.cloudwebmail.net/
 
    */
-  // var connection = {
-  //   host: "http://mail.cloudwebmail.net", //"smtp.gmail.com", // ,
-  //   port: 465,
-  //   secure: true, // use SSL
-  //   auth: {
-  //     user: "salescoordinator@arabtechstore.com",
-  //     pass: "zG&LYE4dp?LA]rJ",
-  //   },
-  //   // logger: true,
-  //   // requireTLS: true, //Force TLS
-  //   // tls: {
-  //   //   rejectUnauthorized: false,
-  //   // },
-  // };
-
   var connection = {
-    host: process.env.SMTP_HOST || "mail.gaviral.in", //"smtp.gmail.com", // ,
-    port: process.env.SMTP_PORT || 26, //465,
+    host: "mail.cloudwebmail.net", //"smtp.gmail.com", // ,
+    port: 465,
+    secure: true,
     auth: {
-      user: process.env.SMTP_USER || "otp@gaviral.in",
-      pass: process.env.SMTP_PASSWORD || "otp@2020",
+      user: "salescoordinator@arabtechstore.com",
+      pass: "zG&LYE4dp?LA]rJ",
     },
     logger: true,
-    requireTLS: true, //Force TLS
-    tls: {
-      rejectUnauthorized: false,
-    },
+    // requireTLS: true, //Force TLS
+    // tls: {
+    //   rejectUnauthorized: false,
+    // },
   };
+
+  // var connection = {
+  //   host: process.env.SMTP_HOST || "mail.gaviral.in", //"smtp.gmail.com", // ,
+  //   port: process.env.SMTP_PORT || 26, //465,
+  //   auth: {
+  //     user: process.env.SMTP_USER || "otp@gaviral.in",
+  //     pass: process.env.SMTP_PASSWORD || "otp@2020",
+  //   },
+  //   logger: true,
+  //   requireTLS: true, //Force TLS
+  //   tls: {
+  //     rejectUnauthorized: false,
+  //   },
+  // };
+  console.log({ connection });
   // Create reusable transporter object using the default SMTP transport
   var transporter = nodemailer.createTransport(connection);
 
   // setup e-mail data
   var mailOptions = {
     //Specify email data
-    from: "Arab Tech Store  <otp@gaviral.in>", //<amitchauhan7890@gmail.com>",
+    from: "salescoordinator@arabtechstore.com", //<amitchauhan7890@gmail.com>",
     //The email to contact
     to: to,
     //Subject and text data
